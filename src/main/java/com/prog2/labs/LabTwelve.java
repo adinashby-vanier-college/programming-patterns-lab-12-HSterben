3027e9b91f08bb131c2804c2f081263a4e43221c
HSterben <142906656+HSterben@users.noreply.github.com>
2023-10-12 13:28:45 -0400

    Update LabTwelve.java
    Idea, not finished

--- src/main/java/com/prog2/labs/LabTwelve.java
+++ src/main/java/com/prog2/labs/LabTwelve.java
@@ -1,29 +1,48 @@
 package com.prog2.labs;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 /**
  * @author adinashby
  *
  */
-
 public class LabTwelve {
 
-	/**
-	 * Write your test code below in the main (optional).
-	 *
-	 */
-	public static void main(String[] args) {
+    /**
+     * Write your test code below in the main (optional).
+     *
+     */
+    public static void main(String[] args) {
 
-	}
+    }
 
-	/**
-	 * Please refer to the README file for question(s) description
-	 */
+    /**
+     * Please refer to the README file for question(s) description
+     */
+    public static int numberOfBoomerangs(int[][] points) {
+        if (points.length != 3) {
+            return 0;
+        }
+        HashSet<Integer> set = new HashSet();
+        
+        int counter = 0;
+        
+        set.add(findDistance(points[0], points[1]));
+        if (!set.add(findDistance(points[0], points[2]))) {
+            System.out.println("false");
+            counter = 1;
+        }
+        if (!set.add(findDistance(points[1], points[2]))) {
+            System.out.println("false again");
+            counter = 2;
+        }
+        return counter;
+    }
 
-	public static int numberOfBoomerangs(int[][] points) {
-		return 0;
-	}
+    public static int findDistance(int[] p1, int[] p2) {
+        return (int) Math.sqrt(Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2));
+    }
 
 }
